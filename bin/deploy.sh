ced6928569f6fd021e81c902bcfeb3232dc93981
Taketo Takashima <t.taketo1113@gmail.com>
2017-01-26 18:09:48 +0900

    Fixed deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,7 @@
 src=/var/www/www.sindan-net.com
 dst=/root/src/www.sindan-net.com/
 
-cd $src
+cd $dst
 git checkout gh-pages
 git pull origin gh-pages
 rsync -avz --exclude='.git/' --chown=www-data:www-data $dst $src
